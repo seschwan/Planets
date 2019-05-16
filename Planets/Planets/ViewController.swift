8a66f8ad07ce3290ce58eafcb8ff4a27853fde7d
Seschwan <seschwanssd@Coreys-iMac.local>
2019-05-15 19:42:26 -0600

    First Commit

---
+++ Planets/Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Seschwan on 5/15/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
