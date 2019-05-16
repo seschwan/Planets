6d6225b2b7bef1930ff9ea285d5cc0784f2c6887
Seschwan <seschwanssd@Coreys-iMac.local>
2019-05-15 20:11:19 -0600

    Setup interface ffor planets

---
+++ Planets/Model/PlanetCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Seschwan on 5/15/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var planetImageView:  UIImageView!
+    @IBOutlet weak var planetLabel:      UILabel!
+    
+    
+}
